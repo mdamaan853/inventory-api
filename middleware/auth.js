a47708247501f871a96d3f14c175f1c9766ee525
amaan2600 <amaan@onlinepartnership.nl>
2024-03-09 14:08:50 +0530

    console.log rm

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,8 @@ const dotenv = require("dotenv")
 dotenv.config()
 module.exports = (req, res, next) => {
     var token = req.headers.token;
-    console.log(token)
     try {
         var decode = jwt.verify(token,process.env.JWT_SECRET_KEY)
-        // console.log(decode)
-        console.log('you are logged in'),
             next();
     } catch {
         res.json({
